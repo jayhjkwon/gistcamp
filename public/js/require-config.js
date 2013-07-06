1aa8709eb29a0877cd8d56893debbec35d4de3d7
Ray H.J Kwon <nicekhj@gmail.com>
2013-07-06 13:18:07 +0900

    added dependencies

--- public/js/require-config.js
+++ public/js/require-config.js
@@ -9,7 +9,13 @@ require.config({
 	    handlebars     : '../vendor/handlebars.js/dist/handlebars',
 	    hbs            : '../vendor/require-handlebars-plugin/hbs',
 	    i18nprecompile : '../vendor/require-handlebars-plugin/hbs/i18nprecompile',
-	    json2          : '../vendor/require-handlebars-plugin/hbs/json2'
+	    json2          : '../vendor/require-handlebars-plugin/hbs/json2',
+	    bootstrap      : '../vendor/bootstrap/docs/assets/js/bootstrap',
+	    prettify       : '../vendor/bootstrap/docs/assets/js/google-code-prettify/prettify',
+	    nicescroll     : '../vendor/jquery-nicescroll/jquery.nicescroll',
+	    autoGrow       : '../vendor/autogrow-textarea/jquery.autogrowtextarea',
+	    scrollTo       : '../vendor/jquery.scrollTo/jquery.scrollTo',
+	    bootmetroPivot : '../vendor/bootmetro/scripts/bootmetro-pivot'
 	},
 	shim: {
 		underscore: {
@@ -25,6 +31,21 @@ require.config({
         },
         handlebars: {
         	exports: 'Handlebars'
+        },
+        bootstrap: {
+        	deps: ['jquery']
+        },
+        nicescroll: {
+        	deps: ['jquery']
+        },
+        autoGrow : {
+        	deps: ['jquery']
+        },
+        scrollTo: {
+        	deps: ['jquery']
+        },
+        bootmetroPivot: {
+        	deps: ['jquery']
         }
 	},
 	hbs : {
