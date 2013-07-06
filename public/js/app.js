ecf5acef4760698627da818d84b91ce61c95b5bf
Ray H.J Kwon <nicekhj@gmail.com>
2013-07-06 14:16:46 +0900

    applied prototype page temporarily

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-require(['jquery', 'backbone', 'views/topView', 'controller', 'router', 'handlebars', 'application'], 
+/*require(['jquery', 'backbone', 'views/topView', 'controller', 'router', 'handlebars', 'application'], 
 	function($, Backbone, TopView, Controller, Router, Handlebars, App){
 	$(function(){
 		App.addRegions({
@@ -29,5 +29,156 @@ require(['jquery', 'backbone', 'views/topView', 'controller', 'router', 'handleb
 		});
 
 		App.start();
+	});
+});*/
+
+
+
+define(function(require){
+	var 
+		$ = require('jquery')
+	;
+
+	require('bootstrap');
+	require('prettify');
+	require('nicescroll');
+	require('autoGrow');
+	require('scrollTo');
+	require('bootmetroPivot');
+
+
+	$(function(){
+		$('.gist-list').niceScroll({cursorcolor: '#eee'});
+		$('.center').niceScroll({cursorcolor: '#eee'});
+		$('.comments-wrapper').niceScroll({cursorcolor: '#eee'});
+
+		$('.btn-comments').tooltip();
+
+		$('.btn-comments').click(function (e) {
+		  	e.preventDefault();
+		  	if($('.comments-wrapper').css('right') == '-300px'){
+		  		$('.center').css('right', '300px');
+		  		$('.comments-wrapper').css('right','0px');	  
+		  		setTimeout(function(){
+			  		$('#comment-input').focus();
+			  	},300);		
+		  	}else{
+		  		$('.center').css('right', '0px');
+		  		$('.comments-wrapper').css('right','-300px');
+		  	}
+
+		  	/*if($('.comments-wrapper').css('margin-left') == '0px'){
+		  		// $('.center').css('right', '300px');
+		  		$('.comments-wrapper').css('margin-left','-300px');	  
+		  		setTimeout(function(){
+			  		$('#comment-input').focus();
+			  	},300);		
+		  	}else{
+		  		// $('.center').css('right', '0px');
+		  		$('.comments-wrapper').css('margin-left','0px');
+		  	}*/
+
+		  	setTimeout(function(){
+		  		$('.center').getNiceScroll().resize();	
+		  	},300);
+		  	
+		});	
+
+	    $('#pivot').pivot();
+
+	    $('.pivot-headers a').click(function(e){
+	    	e.preventDefault();
+	    	// $('.center').niceScroll();
+	    });
+
+	    // listen to slide event completion
+	    $('.pivot').on('slid', function(){
+	    	// Check for scrollbars resize (when content or position have changed)
+	    	$('.center').getNiceScroll().resize();
+	    });
+
+	    $('.gist-item').click(function(){
+			$('.gist-item').removeClass('selected');
+			$(this).toggleClass('selected');
+
+			$('.comments-badge').hide().show(500);
+		});	
+
+		prettyPrint();	
+
+		$('.tag').popover({
+			html	: true,
+			trigger : 'click',
+			placement: 'top',
+			title	: '<div><i class="icon-plus"></i> Add Tag</div>',
+			content : '<div class="add-tag">' +
+	                      '<ul>' +
+	                        '<li><a href="#">Important <span class="badge badge-inverse pull-right">16</span></a></li>' +
+	                        '<li><a href="#">JavaScript <span class="badge badge-inverse pull-right">6</span></a></li>' +
+	                        '<li><a href="#">C# <span class="badge badge-inverse pull-right">1</span></a></li>' +
+	                        '<li><input type="text" placeholder="New Tag" /></li>' +
+	                      '</ul>' +
+	                    '</div>'
+	    });
+
+	    $('#comment-input').autoGrow();
+
+	    // check if elem is visible
+	    var isScrolledIntoView = function(scrollElem, elem) {
+		    var docViewTop = $(scrollElem).scrollTop();
+		    var docViewBottom = docViewTop + $(scrollElem).height();
+
+		    var elemTop = $(elem).offset().top;
+		    var elemBottom = elemTop + $(elem).height();
+
+		    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+		};
+
+	    $(document).keydown(function(e){
+	    	var gistList, selectedGist, selectedGistIndex;
+	    	var keyCode = e.keyCode || e.which;
+
+	    	if (keyCode === 38 || keyCode === 40){
+	    		gistList = $('.gist-list .gist-item');
+	    		selectedGist = $('.gist-list .gist-item.selected');
+	    		selectedGistIndex = gistList.index(selectedGist);
+	    	}
+
+	    	switch (keyCode) 
+	    	{
+				case 38 : 	// arrow-up key
+		    		if (selectedGistIndex === 0)
+		    			return;
+
+		    		selectedGist.removeClass('selected');
+		    		var prevGist = gistList[selectedGistIndex - 1];
+		    		$(prevGist).addClass('selected');
+		    		if (!isScrolledIntoView('.gist-list', '.gist-list .gist-item.selected'))
+		    			$('.gist-list').scrollTo($(prevGist));
+
+		    		break;
+				case 40 : 	// arrow-down key
+		    		if (gistList.length === (selectedGistIndex + 1))
+		    			return;
+
+		    		selectedGist.removeClass('selected');
+		    		var nextGist = gistList[selectedGistIndex + 1];
+		    		$(nextGist).addClass('selected');
+		    		if (!isScrolledIntoView('.gist-list', '.gist-list .gist-item.selected'))
+		   				$('.gist-list').scrollTo($(nextGist));
+
+		   			break;
+				case 37 : 	// arrow-left key
+					if ( !$('#comment-input').val())
+						$('.btn-comments').trigger('click');
+					break;
+				case 39 : 	// arrow-right key
+					if (!$('#comment-input').val())
+						$('.btn-comments').trigger('click');
+					break;
+				default:
+					break;
+			}
+		});
 	});
 });
