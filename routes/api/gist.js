30901a3c7b266fafe2d344442d1a7846ae4605b5
Ray H.J Kwon <nicekhj@gmail.com>
2013-07-20 07:51:08 +0900

    added github wrapper api called node-github

--- routes/api/gist.js
+++ routes/api/gist.js
@@ -9,30 +9,7 @@ exports.getGistList = function(req, res){
 		page: 1,
 		per_page: 30
 	},
-		function(err, data){
-			res.send(data);
-		});
-};
-
-
-/*var 
-	_      = require('underscore'),
-	github = require('octonode')
-;*/
-
-/* for authenticated access */
-/*var	client = github.client({
-	username: 'RayKwon',
-	password: 'userpassword'
-});*/
-
-/* public access */
-/*var client = github.client();
-
-exports.getGistList = function(req, res){
-	var ghgist = client.gist();
-	ghgist.list({page:1, per_page:100}, function(err, data){
+	function(err, data){
 		res.send(data);
-	});	
-};*/
-
+	});
+};
