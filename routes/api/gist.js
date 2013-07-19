1a43b49f283ec04da3f042b4965fb5ce8d31d48b
Ray H.J Kwon <nicekhj@gmail.com>
2013-07-20 07:50:31 +0900

    added github wrapper api called node-github

--- routes/api/gist.js
+++ routes/api/gist.js
@@ -1,7 +1,24 @@
-var 
+var GitHubApi = require('github');
+
+var github = new GitHubApi({
+	version: '3.0.0'
+});
+
+exports.getGistList = function(req, res){
+	github.gists.getAll({
+		page: 1,
+		per_page: 30
+	},
+		function(err, data){
+			res.send(data);
+		});
+};
+
+
+/*var 
 	_      = require('underscore'),
 	github = require('octonode')
-;
+;*/
 
 /* for authenticated access */
 /*var	client = github.client({
@@ -10,12 +27,12 @@ var
 });*/
 
 /* public access */
-var client = github.client();
+/*var client = github.client();
 
 exports.getGistList = function(req, res){
 	var ghgist = client.gist();
 	ghgist.list({page:1, per_page:100}, function(err, data){
 		res.send(data);
 	});	
-};
+};*/
 
