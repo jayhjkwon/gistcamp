368784b8e331a2cceb0430fead4eb6c339cd20ee
RayKwon <nicekhj@gmail.com>
2013-07-28 11:49:38 +0900

    show number of comments

--- public/js/views/footerView.js
+++ public/js/views/footerView.js
@@ -1,11 +1,29 @@
 define(function(require){
 	var
-		Marionette = require('marionette'),
-		footerTemplate = require('hbs!templates/footerTemplate'),
+		Marionette      = require('marionette'),
+		footerTemplate  = require('hbs!templates/footerTemplate'),
+		postalWrapper   = require('postalWrapper'),
+		constants 		= require('constants'),		
 
 		FooterView = Marionette.Layout.extend({
 			className: 'command-buttons',
-			template : footerTemplate
+			template : footerTemplate,
+			
+			initialize: function(){
+				this.subscription = postalWrapper.subscribe(constants.GIST_ITEM_SELECTED, this.onItemSelected);
+			},
+
+			onItemSelected : function(gistItem){
+				if (gistItem && gistItem.comments > 0){
+					$('.comments-badge').text(gistItem.comments).show();
+				}else{
+					$('.comments-badge').text('').hide();
+				}
+			},
+
+			close: function(){
+				this.subscription.unsubscribe();
+			}
 		})
 	;
 
