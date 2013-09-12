2bdbca02cea36321740baff6b9b9386214b86106
Geun-Young <kgysmart@gmail.com>
2013-09-12 22:23:13 +0900

    merge

--- public/js/models/gistItem.js
+++ public/js/models/gistItem.js
@@ -1,7 +1,14 @@
 define(function(require){
 	var
 		Backbone		= require('backbone'),		
-		GistItem 		= Backbone.Model.extend({		
+		GistItem 		= Backbone.Model.extend({	
+
+			initialize: function(props){
+				// console.log('gistItem initialized');
+				// this.gistId = props ? props.gistId || '' : '';
+				// this.id = props ? props.id || '' : '';
+			},
+
 			url : function(){
 				return '/api/gist/newgist';
 			}	
